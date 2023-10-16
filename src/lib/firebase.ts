c5b6779aa40101fa313aff1e60710be68e36e34b
Skarf <54850873+Skarf4U@users.noreply.github.com>
2023-10-16 03:01:19 +0300

    Initial commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "notechamar.firebaseapp.com",
+  projectId: "notechamar",
+  storageBucket: "notechamar.appspot.com",
+  messagingSenderId: "477378857077",
+  appId: "1:477378857077:web:94abaa9682b2184260503c",
+  measurementId: "G-ZTQFNWK7XT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
